291ea0edfd265fa301dc147a84aa7fc03826d30c
Ewaldjebaraj <ewaldjebaraj.sf3401@gmail.com>
2022-06-24 15:21:39 +0530

    Avatar view getting started samples

---
+++ AvatarViewGettingStarted/AvatarViewGettingStarted/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace AvatarViewGettingStarted;
+
+public partial class MainPage : ContentPage
+{
+	
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ AvatarViewGettingStarted/AvatarViewGettingStarted/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace AvatarViewGettingStarted;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
